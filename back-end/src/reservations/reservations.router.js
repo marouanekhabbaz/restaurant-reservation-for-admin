9e8db2eb9c51a6a2c5d42a7ae3dca2db124d5cbc
marouane khabbaz <77408531+marouanekhabbaz@users.noreply.github.com>
2021-07-11 12:42:58 -0400

    add method not allowed

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -6,8 +6,9 @@
 
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/:reservationId/status").get(controller.read).put(controller.update)
-router.route("/:reservationId").get(controller.read).put(controller.updatewholeReservation)
-router.route("/").get(controller.list).post(controller.create)
+router.route("/:reservationId/status").get(controller.read).put(controller.update).all(methodNotAllowed);
+router.route("/:reservationId").get(controller.read).put(controller.updatewholeReservation).all(methodNotAllowed);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 module.exports = router;
--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -2,8 +2,9 @@
 
 const router = require("express").Router();
 const controller = require("./tables.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).post(controller.create)
-router.route("/:tableId/seat").get(controller.read).put(controller.update).delete(controller.delete)
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
+router.route("/:tableId/seat").get(controller.read).put(controller.update).delete(controller.delete).all(methodNotAllowed);
 
 module.exports = router;
